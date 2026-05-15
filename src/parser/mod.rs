d5a3e8f22595acec4364e3049b8851451ccbf820
ben-toker <117331544+ben-toker@users.noreply.github.com>
2026-05-15 19:48:48 -0400

    exposed public api for parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -5,3 +5,9 @@ mod lexer;
 pub use ast::Expr;
 pub use lexer::Token;
 pub use lexer::tokenize;
+
+pub fn parse(input: &str) -> Result<Expr, String> {
+    let tokens = lexer::tokenize(input)?;
+
+    return grammar::parse_tokens(tokens);
+}
